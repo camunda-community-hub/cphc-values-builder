8e2048c7a39addcb3cd21af638acdd26484032c0
Jesse Simpson <jesse.simpson@camunda.com>
2025-11-15 13:43:06 -0500

    fix: go back to using monospace font in code snippet
    Signed-off-by: Jesse Simpson <jesse.simpson@camunda.com>

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
-//import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css'
+import { Inter, IBM_Plex_Mono } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const ibmPlexMono = IBM_Plex_Mono({ 
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+  variable: '--font-ibm-plex-mono'
+})
 
 export const metadata = {
   title: 'Create values.yaml',
@@ -10,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={ibmPlexMono.variable}>
       <body className={inter.className}>{children}</body>
     </html>
   )
